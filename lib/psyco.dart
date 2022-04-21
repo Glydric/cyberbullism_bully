8ceedc3157955be37575c711309724aa7b07a84d
leomiglio02 <56474420+leomiglio02@users.noreply.github.com>
2022-04-21 15:06:00 +0200

    Added Chosable id

--- lib/psyco.dart
+++ lib/psyco.dart
@@ -5,38 +5,41 @@ import 'package:html/parser.dart';
 import 'package:http/http.dart' as http;
 
 class Psyco {
-  final url =
-      "https://areariservata.psy.it/cgi-bin/areariservata/albo_nazionale.cgi";
   Map map = HashMap<dynamic, dynamic>();
 
-  Psyco.fromAlbo(String nome, String cognome, String ordine) {
-    getPsyco(nome, cognome, ordine);
+  Psyco.fromAlbo(String nome, String cognome, String ordine, int i) {
+    getPsyco(nome, cognome, ordine, i);
   }
 
-  getPsyco(String nome, String cognome, String ordine) async =>
-      http.post(Uri.parse(url), body: {
-        'name': nome,
-        'cognome': cognome,
-        'ordine': ordine,
-        'azione': 'cerca'
-      }).then((http.Response res) {
-        if (res.statusCode < 200 || 400 < res.statusCode) {
-          throw new Exception("Error while fetching data");
-        }
+  getPsyco(String nome, String cognome, String ordine, int i) async =>
+      http.post(
+        Uri.parse(
+            "https://areariservata.psy.it/cgi-bin/areariservata/albo_nazionale.cgi"),
+        body: {
+          'name': nome,
+          'cognome': cognome,
+          'ordine': ordine,
+          'azione': 'cerca'
+        },
+      ).then(
+        (http.Response res) {
+          if (res.statusCode < 200 || 400 < res.statusCode) {
+            throw new Exception("Error while fetching data");
+          }
+          urlParser(i, res.body);
+        },
+      );
 
-        urlParse(res.body);
-      });
-
-  urlParse(String body) {
+  urlParser(int i, String body) {
     var a = parse(body)
         .getElementsByClassName("testo_small")[0]
         .getElementsByTagName("td");
-    map["cognome"] = getElement(a[0], '');
-    map["nome"] = getElement(a[1], '');
-    map["pec"] = a[2].attributes['data-pec'].toString();
-    map["ordine"] = getElement(a[3], 'a');
-    map["sezione"] = getElement(a[4], 'b');
-    map["isValid"] = getElement(a[6], 'font').isEmpty;
+    map["cognome"] = getElement(a[0 + 8 * i], '');
+    map["nome"] = getElement(a[1 + 8 * i], '');
+    map["pec"] = a[2 + 8 * i].attributes['data-pec'].toString();
+    map["ordine"] = getElement(a[3 + 8 * i], 'a');
+    map["sezione"] = getElement(a[4 + 8 * i], 'b');
+    map["isValid"] = getElement(a[6 + 8 * i], 'font').isEmpty;
   }
 
   String getElement(Element e, String tagName) {
