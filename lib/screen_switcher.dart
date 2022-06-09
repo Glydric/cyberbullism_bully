a5f7c4126e59174f0b88ee9218941edf4d5531e7
leomiglio02 <56474420+leomiglio02@users.noreply.github.com>
2022-06-09 15:46:30 +0200

    Beautify

--- lib/screen_switcher.dart
+++ lib/screen_switcher.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'example_home_page.dart';
 import 'login/login_screen.dart';
 
-
 class ScreenSwitcher extends StatefulWidget {
   const ScreenSwitcher({Key? key}) : super(key: key);
 
@@ -21,9 +20,7 @@ class _ScreenSwitcherState extends State<ScreenSwitcher> {
   ];
 
   void _updateIndex(int value) {
-    setState(() {
-      _index = value;
-    });
+    setState(() => _index = value);
   }
 
   @override
@@ -35,10 +32,18 @@ class _ScreenSwitcherState extends State<ScreenSwitcher> {
         currentIndex: _index,
         onTap: _updateIndex,
         items: const [
-          BottomNavigationBarItem(label: "Home", icon: Icon(Icons.home)),
-          BottomNavigationBarItem(label: "Home", icon: Icon(Icons.home)),
           BottomNavigationBarItem(
-              label: "Add", icon: Icon(Icons.account_circle_outlined)),
+            label: "Learning (In work)",
+            icon: Icon(Icons.description),
+          ),
+          BottomNavigationBarItem(
+            label: "Home (In work)",
+            icon: Icon(Icons.dangerous),
+          ),
+          BottomNavigationBarItem(
+            label: "User (half in work)",
+            icon: Icon(Icons.account_circle_outlined),
+          ),
         ],
       ),
     );
