d50c593875fae0fdf73b37a1655c7896e09b795d
leomiglio02 <56474420+leomiglio02@users.noreply.github.com>
2022-07-14 23:35:36 +0200

    builder modified with lambda

--- lib/psyco/psyco_page.dart
+++ lib/psyco/psyco_page.dart
@@ -15,30 +15,29 @@ class PsyInfo extends StatefulWidget {
 }
 
 class _PsyInfoState extends State<PsyInfo> {
+  _getPsyInfo(Psyco psy) => ListView(
+        padding: const EdgeInsets.all(20),
+        children: [
+          Text(psy.nome),
+          Text(psy.cognome),
+          Text(psy.ordine),
+          Text(psy.sezione),
+          Text(psy.isValid),
+          Text(psy.email),
+          Text(psy.pageUrl),
+        ],
+      );
   @override
   Widget build(BuildContext context) => Scaffold(
         appBar: AppBar(title: const Text("User info")),
         body: Center(
           child: FutureBuilder<Psyco>(
-            future: PsycoUrlGetter.getFuturePsyco(
-                widget.nome, widget.cognome, widget.ordine, 0),
-            builder: (BuildContext context, AsyncSnapshot<Psyco> snapshot) {
-              if (!snapshot.hasData) return const CircularProgressIndicator();
-              Psyco psy = snapshot.requireData;
-              return ListView(
-                padding: const EdgeInsets.all(20),
-                children: [
-                  Text(psy.nome),
-                  Text(psy.cognome),
-                  Text(psy.ordine),
-                  Text(psy.sezione),
-                  Text(psy.isValid),
-                  Text(psy.email),
-                  Text(psy.pageUrl),
-                ],
-              );
-            },
-          ),
+              future: PsycoUrlGetter.getFuturePsyco(
+                  widget.nome, widget.cognome, widget.ordine, 0),
+              builder: (BuildContext context, AsyncSnapshot<Psyco> snapshot) =>
+                  snapshot.hasData
+                      ? _getPsyInfo(snapshot.requireData)
+                      : const CircularProgressIndicator()),
         ),
       );
 }
