2fcca0c3ecd409bc865e461fd132546db4c82e03
leomiglio02 <56474420+leomiglio02@users.noreply.github.com>
2022-07-25 19:00:08 +0200

    added methods

---
+++ lib/Model/BdConnectors/db_psy_connector.dart
@@ -0,0 +1,17 @@
+import '/Model/psyco/psyco.dart';
+
+class DbPsyConnector {
+  static addPsy(Psyco psi, int firebaseID) {
+    throw Exception("Metodo non definito\npsicologo: " +
+        psi.toString() +
+        "\nfirebaseID:" +
+        firebaseID.toString());
+  }
+
+  static modifyPsyPassword(Psyco psi, String newPassword) {
+    throw Exception("Metodo non definito\nuser: " +
+        psi.toString() +
+        "\n nuova password: " +
+        newPassword);
+  }
+}
