c790f462b1d5fc17a2ed4d8553bbfc7a205d0d4e
loris minetti <lorisminetti1997@gmail.com>
2022-08-02 19:21:01 +0200

    Lista_Segnalazioni Scaffold

--- lib/View/segnalazioni/user/lista_segnalazione.dart
+++ lib/View/segnalazioni/user/lista_segnalazione.dart
@@ -6,6 +6,9 @@ class ListaSegnalazione extends StatefulWidget {
   final String messaggio = "";
   final int gravita = 0;
 
+  //Ancora non inizializzata
+  final Widget _segnalazioni = ListViewHome();
+
   const ListaSegnalazione({Key? key}) : super(key: key);
 
   @override
@@ -16,7 +19,32 @@ class ListaSegnalazioneState extends State<ListaSegnalazione> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+        appBar: AppBar(
+            title: const Text("Le tue segnalazioni"),
+            leading: IconButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                icon: const Icon(Icons.arrow_back_ios_new_sharp))),
+        body: ListViewHome(),
+        floatingActionButton: FloatingActionButton(
+            onPressed: (() {
+              debugPrint('Inserire qui la segnalazione "CARD"');
+            }),
+            child: const Icon(Icons.add)));
+  }
+}
+
+class ListViewHome extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      padding: const EdgeInsets.all(8),
+      children: <Widget>[
+        Text('Segnalazione 1'),
+        Text('Segnalazione 2'),
+        Text('Segnalazione 3'),
+      ],
     );
   }
 }
