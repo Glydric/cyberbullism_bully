33612495a6c863eaafad459c2cbdb0a68e765018
leomiglio02 <56474420+leomiglio02@users.noreply.github.com>
2022-08-02 17:39:28 +0200

    cleaned

--- lib/View/segnalazioni/switcher_segnalazioni.dart
+++ lib/View/segnalazioni/switcher_segnalazioni.dart
@@ -2,11 +2,7 @@ import 'package:flutter/material.dart';
 import '../../Model/user_save_manager.dart';
 import '/Model/user.dart';
 import '/View/segnalazioni/psy_segnalazione.dart';
-<<<<<<< Updated upstream:lib/View/segnalazioni/switcher_segnalazioni.dart
 import '/View/segnalazioni/user/lista_segnalazione.dart';
-=======
-import 'user/lista_segnalazione.dart';
->>>>>>> Stashed changes:lib/View/segnalazioni/segnalazioni_page.dart
 
 class SegnalazioniPage extends StatelessWidget {
   User? user;
@@ -18,7 +14,6 @@ class SegnalazioniPage extends StatelessWidget {
     initUser();
     return Scaffold(
         body: user.runtimeType.toString() == "Psyco"
-<<<<<<< Updated upstream:lib/View/segnalazioni/switcher_segnalazioni.dart
             ? const PsycoSegnalazione()
             : const ListaSegnalazione());
   }
@@ -27,9 +22,5 @@ class SegnalazioniPage extends StatelessWidget {
     try {
       user = await UserSavingManager.getUser();
     } catch (e) {}
-=======
-            ? PsycoSegnalazione()
-            : UserSegnalazione());
->>>>>>> Stashed changes:lib/View/segnalazioni/segnalazioni_page.dart
   }
 }
