958a07aaa6968ffd90a1ad9f368c38c04597ac94
leomiglio02 <56474420+leomiglio02@users.noreply.github.com>
2022-08-10 16:14:21 +0200

    begin implementing

--- lib/View/segnalazioni/user/segnalazione_card.dart
+++ lib/View/segnalazioni/user/segnalazione_card.dart
@@ -2,9 +2,13 @@ import 'package:flutter/material.dart';
 
 import '/Model/segnalazione.dart';
 
-class SegnalazioneCard extends Container {
-  SegnalazioneCard(Segnalazione segnalazione) {
-    Container(
+class SegnalazioneCard extends StatelessWidget {
+  Segnalazione segnalazione;
+  SegnalazioneCard(this.segnalazione, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
       child: Text(segnalazione.testo),
     );
   } //TODO implementare il costruttore che prende una segnalazione e crea una card
