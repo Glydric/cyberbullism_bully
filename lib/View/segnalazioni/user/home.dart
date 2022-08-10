3e8b4dc260f5bf16476862f0a3676c8bb9a91169
loris minetti <lorisminetti1997@gmail.com>
2022-08-11 00:00:36 +0200

    List SegnalazioneCard + gravità

--- lib/View/segnalazioni/user/home.dart
+++ lib/View/segnalazioni/user/home.dart
@@ -1,12 +1,12 @@
+import '../../../Model/segnalazione.dart';
 import '/Model/connect_db/user_connector.dart';
 import 'package:flutter/material.dart';
 import '/Model/user.dart';
 import 'card_aggiunta.dart';
 import 'lista.dart';
+import 'segnalazione_card.dart';
 
 class HomeSegnalazioni extends StatefulWidget {
-  final String messaggio = "";
-  final int gravita = 0;
 
   const HomeSegnalazioni({Key? key}) : super(key: key);
 
@@ -17,13 +17,14 @@ class HomeSegnalazioni extends StatefulWidget {
 class _HomeSegnalazioniState extends State<HomeSegnalazioni> {
   bool _isWriting = false;
 
+  
   @override
   Widget build(BuildContext context) => Scaffold(
         body: Center(
           child: Stack(
-            children: <Widget>[
-              ListaSegnalazioni(),
-              _isWriting ? const CardAggiunta() : Container()
+            children: <Widget>[ 
+              _isWriting ? const CardAggiunta() : Container(),
+              ListaSegnalazioni()
             ],
           ),
         ),
