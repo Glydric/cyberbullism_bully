c9cddf96242ff7ea84815e445e85082e4f856036
leomiglio02 <56474420+leomiglio02@users.noreply.github.com>
2022-08-10 16:12:57 +0200

    begin implementation

--- lib/View/segnalazioni/user/segnalazione_card.dart
+++ lib/View/segnalazioni/user/segnalazione_card.dart
@@ -4,6 +4,8 @@ import '/Model/segnalazione.dart';
 
 class SegnalazioneCard extends Container {
   SegnalazioneCard(Segnalazione segnalazione) {
-
+    Container(
+      child: Text(segnalazione.testo),
+    );
   } //TODO implementare il costruttore che prende una segnalazione e crea una card
 }
