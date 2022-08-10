0c17e4722c0839a774923d86056734aa72e3aa24
leomiglio02 <56474420+leomiglio02@users.noreply.github.com>
2022-08-10 16:04:34 +0200

    added Segnalazione Card

--- lib/View/segnalazioni/user/lista.dart
+++ lib/View/segnalazioni/user/lista.dart
@@ -1,3 +1,4 @@
+import 'segnalazione_card.dart';
 import '/Model/segnalazione.dart';
 import '/Model/connect_db/user_connector.dart';
 import 'package:flutter/material.dart';
@@ -5,7 +6,7 @@ import '/Model/user.dart';
 
 class ListaSegnalazioni extends StatelessWidget {
   //Ancora non inizializzata
-  final _list = <Segnalazione>[];
+  final _list = <SegnalazioneCard>[];
   //TODO lista di segnalazioni
 
   ListaSegnalazioni({Key? key}) : super(key: key);
@@ -15,9 +16,7 @@ class ListaSegnalazioni extends StatelessWidget {
     //TODO qui va inserito il getter di list
     return ListView(
       padding: const EdgeInsets.all(8),
-      children: <Widget>[
-        Text(""),
-      ],
+      children: _list
     );
   }
 }
---
+++ lib/View/segnalazioni/user/segnalazione_card.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class SegnalazioneCard extends Container {
+  SegnalazioneCard();//TODO implementare il costruttore che prende una segnalazione e crea una card
+}
