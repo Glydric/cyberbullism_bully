e506f2903d836288151c0ea1d47305da104230aa
leomiglio02 <56474420+leomiglio02@users.noreply.github.com>
2022-08-12 14:28:03 +0200

    simplify fillCard

--- lib/View/segnalazioni/user/lista.dart
+++ lib/View/segnalazioni/user/lista.dart
@@ -13,28 +13,27 @@ class ListaSegnalazioni extends StatelessWidget {
   List<Segnalazione> lista =
       DbPsyConnector.getSegnalazioni() as List<Segnalazione>;
 
-  ListaSegnalazioni(this.lista, {Key? key}) : super(key: key);
+  ListaSegnalazioni({Key? key}) : super(key: key);
 
-  List<SegnalazioneCard> fillCard(List<Segnalazione> myList) {
-    //ci dovrebbe essere il metodo List.map da vedere meglio..
-    List<SegnalazioneCard> temp = <SegnalazioneCard>[];
-    if (myList.isNotEmpty) {
-      for (int i = 0; i < lista.length; i++) {
-        temp[i].segnalazione.orario = myList[i].orario;
-        temp[i].segnalazione.gravita = myList[i].gravita;
-        temp[i].segnalazione.testo = myList[i].testo;
-        temp[i].segnalazione.email = myList[i].email;
-      }
-    }
-    return temp;
-  }
+  List<SegnalazioneCard> fillCard(List<Segnalazione> myList) =>
+      myList.map(SegnalazioneCard.new).toList();
+  //ci dovrebbe essere il metodo List.map da vedere meglio..
+  // List<SegnalazioneCard> temp = <SegnalazioneCard>[];
+  // if (myList.isNotEmpty) {
+  //   for (int i = 0; i < lista.length; i++) {
+  //     temp[i].segnalazione.orario = myList[i].orario;
+  //     temp[i].segnalazione.gravita = myList[i].gravita;
+  //     temp[i].segnalazione.testo = myList[i].testo;
+  //     temp[i].segnalazione.email = myList[i].email;
+  //   }
+  // }
+  // return temp;
 
   @override
   Widget build(BuildContext context) {
-    
     List<SegnalazioneCard> _list = fillCard(lista);
-    
-    // lista anche relativamente lunga e che 
+
+    // lista anche relativamente lunga e che
     // riserva lo stesso spazio per ogni Container (segnalazione)
 
     return ListView.builder(
@@ -45,8 +44,7 @@ class ListaSegnalazioni extends StatelessWidget {
             color: Color.fromARGB(214, 255, 255, 255),
             child: Center(child: Text('${_list[index]}')),
           );
-        }
-        );
+        });
 
     // return ListView(
     //     scrollDirection: Axis.vertical,
