d5650cf6476b140a1899014d6c5d6935e363ea32
leomiglio02 <56474420+leomiglio02@users.noreply.github.com>
2022-08-13 23:28:31 +0200

    removed old comments

--- lib/View/segnalazioni/psyco/lista.dart
+++ lib/View/segnalazioni/psyco/lista.dart
@@ -2,30 +2,15 @@ import 'package:flutter/material.dart';
 
 import 'segnalazione_card.dart';
 
-
+/// lista anche relativamente lunga e che
+/// riserva lo stesso spazio per ogni Container (segnalazione)
 class ListaSegnalazioni extends StatelessWidget {
   final List<SegnalazioneCard> _list;
 
   const ListaSegnalazioni(this._list, {Key? key}) : super(key: key);
 
-
   @override
-  Widget build(BuildContext context) {
-    // lista anche relativamente lunga e che
-    // riserva lo stesso spazio per ogni Container (segnalazione)
-
-    return ListView.builder(
+  Widget build(BuildContext context) => ListView.builder(
       itemCount: _list.length,
-      itemBuilder: (BuildContext context, int index) => _list[index]
-    );
-
-
-    // return ListView(
-    //     scrollDirection: Axis.vertical,
-    //     padding: const EdgeInsets.all(8),
-    //     children: <SegnalazioneCard>[
-
-    //     ]
-    //     );
-  }
+      itemBuilder: (_, int index) => _list[index]);
 }
