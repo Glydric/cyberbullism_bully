4354431c1d2179b2f20325715a6dd31e33f6715c
leomiglio02 <56474420+leomiglio02@users.noreply.github.com>
2022-08-13 23:08:25 +0200

    removed user from page responsability

--- lib/View/profile/user_page.dart
+++ lib/View/profile/user_page.dart
@@ -5,57 +5,42 @@ import "/Model/user.dart";
 import 'login_registration/change_password.dart';
 
 class UserPage extends StatefulWidget {
-  const UserPage({Key? key}) : super(key: key);
+  final User user;
+  const UserPage(this.user, {Key? key}) : super(key: key);
 
   @override
   State<UserPage> createState() => _UserPageState();
 }
 
 class _UserPageState extends State<UserPage> {
-  User? user;//TODO move user getter to superior page
-
-  void initUser() async {
-    try {
-      user = await UserSavingManager.getUser();
-      setState(() => user);
-    } catch (e) {
-      debugPrint(e.toString());
-    }
-  }
-
   @override
-  Widget build(BuildContext context) {
-    initUser();
-    return Scaffold(
-      appBar: AppBar(title: const Text("Utente")),
-      body: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            children: [
+  Widget build(BuildContext context) => Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            const Spacer(),
+            Text(
+              widget.user.nome + " " + widget.user.cognome,
+              style: const TextStyle(fontSize: 20),
+            ),
+            Text(widget.user.email),
+            const Spacer(
+              flex: 3,
+            ),
+            Row(children: [
+              const ElevatedButton(
+                onPressed: UserSavingManager.removeUser,
+                child: Text("Logout"),
+              ),
               const Spacer(),
-              Text(
-                user!.nome + " " + user!.cognome,
-                style: const TextStyle(fontSize: 20),
+              ElevatedButton(
+                onPressed: () => toPage(ChangePassword(widget.user)),
+                child: const Text("Change Password"),
               ),
-              Text(user!.email),
-              const Spacer(
-                flex: 3,
-              ),
-              Row(children: [
-                const ElevatedButton(
-                  onPressed: UserSavingManager.removeUser,
-                  child: Text("Logout"),
-                ),
-                const Spacer(),
-                ElevatedButton(
-                  onPressed: () => toPage(ChangePassword(user!)),
-                  child: const Text("Change Password"),
-                ),
-              ]),
-            ],
-          )),
-    );
-  }
+            ]),
+          ],
+        ),
+      );
 
   toPage(Widget page) => Navigator.of(context).push(
         MaterialPageRoute(builder: (context) => page),
