76a4ff24c657009fe0ca152b219ae4e8c86ed1fb
leomiglio02 <56474420+leomiglio02@users.noreply.github.com>
2022-08-16 17:56:19 +0200

    switcher now contains the user page infos

--- lib/View/profile/switcher.dart
+++ lib/View/profile/switcher.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import '/Model/user.dart';
 import '/Model/user_save_manager.dart';
 import 'login_registration/login_page.dart';
-import 'user_page.dart';
+import 'login_registration/change_password.dart';
 
 class LoginPageSwitcher extends StatefulWidget {
   const LoginPageSwitcher({Key? key}) : super(key: key);
@@ -13,9 +13,28 @@ class LoginPageSwitcher extends StatefulWidget {
 }
 
 class _LoginPageSwitcherState extends State<LoginPageSwitcher> {
-  toPage(Widget page) => Navigator.of(context)
-      .push(MaterialPageRoute(builder: (context) => page))
-      .whenComplete(() => setState(() {}));
+  logoutAlertDialog() async {
+    var result = await showDialog(
+        context: context,
+        builder: (_) => AlertDialog(
+              title: const Text("Eseguire il logout?"),
+              actions: [
+                MaterialButton(
+                  onPressed: () => Navigator.pop(context, false),
+                  child: const Text("No"),
+                ),
+                MaterialButton(
+                  onPressed: () => Navigator.pop(context, true),
+                  // UserSavingManager.removeUser,
+                  child: const Text("Si"),
+                ),
+              ],
+            ));
+    if (result == true) {
+      UserSavingManager.removeUser();
+      setState(() {});
+    }
+  }
 
   @override
   Widget build(BuildContext context) => Scaffold(
@@ -24,18 +43,46 @@ class _LoginPageSwitcherState extends State<LoginPageSwitcher> {
           future: UserSavingManager.getUser(),
           builder: (BuildContext context, AsyncSnapshot<User> snapshot) =>
               snapshot.hasData
-                  ? UserPage(snapshot.requireData)
+                  ? Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(children: [
+                        const Spacer(),
+                        Text(
+                          snapshot.requireData.nome +
+                              " " +
+                              snapshot.requireData.cognome,
+                          style: const TextStyle(fontSize: 20),
+                        ),
+                        Text(snapshot.requireData.email),
+                        const Spacer(
+                          flex: 3,
+                        ),
+                        Row(children: [
+                          ElevatedButton(
+                            onPressed: logoutAlertDialog,
+                            child: const Text("Logout"),
+                          ),
+                          const Spacer(),
+                          ElevatedButton(
+                            onPressed: () => showMyBottomSheet(
+                                ChangePassword(snapshot.requireData)),
+                            child: const Text("Change Password"),
+                          ),
+                        ])
+                      ]),
+                    )
                   : Center(
                       child: ElevatedButton(
-                        onPressed: () {
-                          showModalBottomSheet(
-                            context: context,
-                            builder: (_) => const LogInPage(),
-                          ).whenComplete(() => setState(() {}));
-                        },
+                        onPressed: () => showMyBottomSheet(const LogInPage()),
                         child: const Text("LogIn"),
                       ),
                     ),
         ),
       );
+
+  void showMyBottomSheet(Widget page) => showModalBottomSheet(
+        backgroundColor: const Color.fromARGB(0, 0, 0, 0), // this is transparent
+        context: context,
+        builder: (_) => page,
+      ).whenComplete(() => setState(() {}));
 }
