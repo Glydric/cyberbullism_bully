655113517a3f944bfd73a215bb672dab70c4871e
leomiglio02 <56474420+leomiglio02@users.noreply.github.com>
2022-08-19 11:58:03 +0200

    resolved bug and completed with expansion and maxlength

--- lib/View/segnalazioni/user/card_aggiunta.dart
+++ lib/View/segnalazioni/user/card_aggiunta.dart
@@ -11,41 +11,36 @@ class CardAggiunta extends StatefulWidget {
 
 ///Classe che definisce l'interfaccia per l'aggiunta di una segnalazione
 class _CardAggiuntaState extends State<CardAggiunta> {
-  final TextEditingController myController = TextEditingController();
-
+  final TextEditingController _textController = TextEditingController();
 
   @override
   void dispose() {
     // Clean up the controller when the widget is removed from the
     // widget tree.
-    myController.dispose();
+    _textController.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) => Container(
         alignment: Alignment.center,
-        //La card l'ho definita come un Expanded Widget, in modo da prendere tutto lo spazio
-        //del suo container.. Possibile aspetto da rivedere. Forse è completamente da rimuovere
-          child: Expanded (child: Card( 
-            elevation: 2,
-            color: const Color.fromARGB(197, 255, 153, 0),
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              //La FittedBox tiene conto della grandezza del widget che c'è dentro
-              //e si autodimensiona
-              child: FittedBox(
-                //.cover è la modalità col quale si espande all'interno dello spazio concesso
-                fit: BoxFit.cover,
-                child: TextFormField(
-                    controller: myController,
-                    keyboardType: TextInputType.multiline,
-                    autocorrect: false,
-                    decoration: const InputDecoration(
-                        label: Text("Digitare un max di 5000 caratteri"))),
+        child: Card(
+          elevation: 2,
+          color: const Color.fromARGB(197, 255, 153, 0),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextFormField(
+              maxLength: 5000,
+              minLines: 4,
+              maxLines: 10,
+              controller: _textController,
+              keyboardType: TextInputType.multiline,
+              autocorrect: false,
+              decoration: const InputDecoration(
+                label: Text("Scrivere una segnalazione"),
               ),
             ),
           ),
-  ),
-  );
+        ),
+      );
 }
