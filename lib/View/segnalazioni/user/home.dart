835519ad15fdfbc445d0ed4ecc3e4e3236c3794e
leomiglio02 <56474420+leomiglio02@users.noreply.github.com>
2022-08-19 13:37:58 +0200

    minors

--- lib/View/segnalazioni/user/home.dart
+++ lib/View/segnalazioni/user/home.dart
@@ -17,11 +17,11 @@ class _UserSegnalazioneState extends State<UserSegnalazione> {
   Widget build(BuildContext context) => Scaffold(
         body: ListaChat(),
         floatingActionButton: FloatingActionButton(
-          onPressed: () => showDialog(
-            context: context,
-            builder: (_) => CardAggiunta(widget.user.email),
-          ),
-          child: const Icon(Icons.add),
-        ),
+            child: const Icon(Icons.add),
+            onPressed: () => showDialog(
+                  context: context,
+                  builder: (_) => CardAggiunta(widget.user.email),
+                )
+            ),
       );
 }
