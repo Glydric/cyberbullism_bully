1369e55b8e6312b0d81e4e704e592bde89ae1231
leomiglio02 <56474420+leomiglio02@users.noreply.github.com>
2022-08-21 11:26:40 +0200

    used user.crypt

--- test/crypt.dart
+++ test/crypt.dart
@@ -1,9 +1,11 @@
 import 'dart:convert';
 import 'package:crypto/crypto.dart';
+import 'package:cyberbullism_bully/Model/user.dart';
 
 Digest hash(List<int> list) => sha256.convert(list);
 
 List<int> utf(String text) => utf8.encode(text);
 void main() {
-  print(hash(utf("ciao")));
+  // ignore: avoid_print
+  print(User.crypt("ciao"));
 }
