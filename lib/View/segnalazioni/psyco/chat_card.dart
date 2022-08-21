4d00416de033a3773b80ec099fb620d479aa64bd
leomiglio02 <56474420+leomiglio02@users.noreply.github.com>
2022-08-21 11:26:27 +0200

    added constructor

--- lib/View/segnalazioni/psyco/chat_card.dart
+++ lib/View/segnalazioni/psyco/chat_card.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class ChatCard extends StatelessWidget{
+  const ChatCard({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
