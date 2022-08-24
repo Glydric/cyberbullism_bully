ce8aac859f2bd398e1f0dfe3b566d5e1b46b0572
leomiglio02 <56474420+leomiglio02@users.noreply.github.com>
2022-08-24 19:39:40 +0200

    added message management

--- lib/View/segnalazioni/user/home.dart
+++ lib/View/segnalazioni/user/home.dart
@@ -1,8 +1,13 @@
-import 'package:cyberbullism_bully/View/segnalazioni/user/lista_chat.dart';
-
-import '/Model/user.dart';
-import 'card_aggiunta.dart';
 import 'package:flutter/material.dart';
+
+import '/Model/connect_db/user_db_connector.dart';
+import '/Model/user.dart';
+import '../../../Model/chat/chat.dart';
+import '/Model/chat/message.dart';
+
+import 'chat/lista_chat.dart';
+import 'chat/chat_card.dart';
+import 'card_aggiunta.dart';
 
 class UserSegnalazione extends StatefulWidget {
   final User user;
@@ -13,15 +18,40 @@ class UserSegnalazione extends StatefulWidget {
 }
 
 class _UserSegnalazioneState extends State<UserSegnalazione> {
+  Future<List<ChatCard>> getChats() async {
+    List<Message> list = await UserDbConnector.getMessagesOf(widget.user);
+    String oldEmail = list[0].otherEmail;
+
+    List<Chat> newList = <Chat>[];
+    int _index = 0;
+
+    for (Message m in list) {
+      if (oldEmail == m.otherEmail) {
+        newList[_index].messages.add(m);
+      } else {
+        newList.add(Chat());
+      }
+
+      oldEmail = m.otherEmail;
+    }
+
+    return newList.map(ChatCard.new).toList();
+  }
+
   @override
   Widget build(BuildContext context) => Scaffold(
-        body: ListaChat(),
+        body: FutureBuilder(
+          future: getChats(),
+          builder: (_, AsyncSnapshot<List<ChatCard>> snapshot) =>
+              snapshot.hasData
+                  ? ListaChat(snapshot.requireData)
+                  : const CircularProgressIndicator.adaptive(),
+        ),
         floatingActionButton: FloatingActionButton(
             child: const Icon(Icons.add),
             onPressed: () => showDialog(
                   context: context,
                   builder: (_) => CardAggiunta(widget.user.email),
-                )
-            ),
+                )),
       );
 }
