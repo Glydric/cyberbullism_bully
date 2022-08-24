500ffe14938d2743a88d2ce0af2b3dcdf9539ba4
leomiglio02 <56474420+leomiglio02@users.noreply.github.com>
2022-08-24 19:38:52 +0200

    list thaat use chatcard created

---
+++ lib/View/segnalazioni/user/chat/lista_chat.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import 'chat_card.dart';
+
+class ListaChat extends StatefulWidget {
+  final List<ChatCard> chats;
+
+  const ListaChat(this.chats, {Key? key}) : super(key: key);
+
+  @override
+  State<ListaChat> createState() => _ListaChatState();
+}
+
+class _ListaChatState extends State<ListaChat> {
+  @override
+  Widget build(BuildContext context) => GestureDetector(
+    // onTap: (value) => Message,
+    child: ListView.builder(
+          itemCount: widget.chats.length,
+          itemBuilder: (_, int _index) => widget.chats[_index],
+        ),
+  );
+  //TODO lista chat
+}
