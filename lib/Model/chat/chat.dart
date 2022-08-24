05cf6f61bc613e3863add3d1abadfa468ce85b59
leomiglio02 <56474420+leomiglio02@users.noreply.github.com>
2022-08-24 19:38:03 +0200

    created a chat

---
+++ lib/Model/chat/chat.dart
@@ -0,0 +1,12 @@
+import 'message.dart';
+
+/// Una lista di messaggi con lo stesso psicologo
+class Chat {
+  final List<Message> messages;
+  Chat() : messages = <Message>[];
+
+  Chat.fromList(String userEmail, List<Message> messageList)
+      : messages = messageList
+            .where((message) => message.otherEmail == userEmail)
+            .toList();
+}
