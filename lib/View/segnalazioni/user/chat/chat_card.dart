e1112d40914011d364540debc2ae0d6131180c64
leomiglio02 <56474420+leomiglio02@users.noreply.github.com>
2022-08-24 20:22:11 +0200

    implemented a chatcard

--- lib/View/segnalazioni/user/chat/chat_card.dart
+++ lib/View/segnalazioni/user/chat/chat_card.dart
@@ -9,5 +9,24 @@ class ChatCard extends StatelessWidget {
   const ChatCard(this.chat, {Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) => Card();
+  Widget build(BuildContext context) => GestureDetector(
+        child: Padding(
+          padding: const EdgeInsets.all(4),
+          child: Card(
+            elevation: 8,
+            child: Padding(
+              padding: const EdgeInsets.all(16),
+              child: Column(children: [
+                Row(children: [
+                  Text(chat.messages[0].otherEmail),
+                  const Spacer(),
+                  Text(chat.messages[0].orario),
+                ]),
+                const Text(''),
+                Text(chat.messages[0].text),
+              ]),
+            ),
+          ),
+        ),
+      );
 }
