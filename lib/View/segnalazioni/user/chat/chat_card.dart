15fac7381dfd8e5f000c812ac0d39b466700417d
leomiglio02 <56474420+leomiglio02@users.noreply.github.com>
2022-08-24 19:38:33 +0200

    created a chat card

---
+++ lib/View/segnalazioni/user/chat/chat_card.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+import '../../../../Model/chat/chat.dart';
+
+/// La singola card di una chat
+class ChatCard extends StatelessWidget {
+  final Chat chat;
+
+  const ChatCard(this.chat, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => Container();
+}
