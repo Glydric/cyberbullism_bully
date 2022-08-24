429eec630701713d590452855ede96264a895446
leomiglio02 <56474420+leomiglio02@users.noreply.github.com>
2022-08-24 20:20:22 +0200

    minor

--- lib/Model/chat/chat.dart
+++ lib/Model/chat/chat.dart
@@ -3,6 +3,7 @@ import 'message.dart';
 /// Una lista di messaggi con lo stesso psicologo
 class Chat {
   final List<Message> messages;
+  
   Chat() : messages = <Message>[];
 
   Chat.fromList(String userEmail, List<Message> messageList)
