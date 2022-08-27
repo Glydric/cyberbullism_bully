9fa8ef5cf8d08634923c680232be2a03e8e59248
leomiglio02 <56474420+leomiglio02@users.noreply.github.com>
2022-08-27 10:27:10 +0200

    defined nav to chatview

--- lib/View/segnalazioni/user/chat/lista_chat.dart
+++ lib/View/segnalazioni/user/chat/lista_chat.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'chat_card.dart';
+import 'chat_view.dart';
 
 class ListaChat extends StatefulWidget {
   final List<ChatCard> chats;
@@ -13,10 +14,15 @@ class ListaChat extends StatefulWidget {
 
 class _ListaChatState extends State<ListaChat> {
   @override
-  Widget build(BuildContext context) => GestureDetector(
-    child: ListView.builder(
-          itemCount: widget.chats.length,
-          itemBuilder: (_, int _index) => widget.chats[_index],
+  Widget build(BuildContext context) => ListView.builder(
+        itemCount: widget.chats.length,
+        itemBuilder: (_, int _index) => GestureDetector(
+          onTap: () => toPage(ChatView(widget.chats[_index].chat)),
+          child: widget.chats[_index],
         ),
-  );
+      );
+
+  toPage(Widget page) => Navigator.of(context).push(MaterialPageRoute(
+        builder: (context) => page,
+      ));
 }
