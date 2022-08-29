86ff2224df725bc903692a70ebf14a87209c3208
loris minetti <lorisminetti1997@gmail.com>
2022-08-29 23:09:13 +0200

    Learning card - grid update

---
+++ lib/View/learning/learning_card.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class LearningCard extends StatefulWidget {
+  const LearningCard({Key? key}) : super(key: key);
+
+  @override
+  State<LearningCard> createState() => LearningCardState();
+}
+
+class LearningCardState extends State<LearningCard> {
+  Icon fav = Icon(Icons.favorite_border);
+  bool isntFav = true;
+
+  Widget build(BuildContext context) => Container(
+      child: Padding(
+          padding: const EdgeInsets.all(4),
+          child: Card(
+            elevation: 8,
+            child: GridTile(
+              header: ListTile(
+                leading: IconButton(
+                    icon: fav,
+                    onPressed: () => setState(
+                          () => isntFav
+                              ? Icon(Icons.favorite)
+                              : Icon(Icons.favorite_border),
+                        )),
+                trailing: Icon(Icons.download_rounded),
+              ),
+              child: Center(child: Text(" E = M*C ")),
+            ),
+            color: Color.fromARGB(202, 223, 236, 82),
+          )));
+}
---
+++ lib/View/learning/learning_grid.dart
@@ -0,0 +1,26 @@
+import 'package:cyberbullism_bully/View/learning/learning_card.dart';
+import 'package:flutter/material.dart';
+
+class LearningGrid extends StatefulWidget {
+  final List<LearningCard> _grid;
+
+  LearningGrid(this._grid, {Key? key}) : super(key: key);
+
+  @override
+  State<LearningGrid> createState() => _LearningGridState();
+}
+
+class _LearningGridState extends State<LearningGrid> {
+  final controller = ScrollController();
+
+  @override
+  Widget build(BuildContext context) => 
+      GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2, mainAxisSpacing: 5, crossAxisSpacing: 5),
+        padding: const EdgeInsets.all(20),
+      
+        itemCount: widget._grid.length,
+        itemBuilder: (context, int index) => widget._grid[index]
+      );
+}
