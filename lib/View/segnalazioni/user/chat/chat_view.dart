3ebd85a006747b5cf8e4850b33b271029c33cf3a
leomiglio02 <56474420+leomiglio02@users.noreply.github.com>
2022-08-29 20:55:45 +0200

    implemented

--- lib/View/segnalazioni/user/chat/chat_view.dart
+++ lib/View/segnalazioni/user/chat/chat_view.dart
@@ -1,20 +1,62 @@
 import 'package:flutter/material.dart';
 
+import '/Model/user.dart';
+import '/Model/connect_db/user_db_connector.dart';
 import '/Model/chat/chat.dart';
+import 'message_card.dart';
 
+/// La schermata che consente di portare avanti una chat con un utente
 class ChatView extends StatefulWidget {
-  String userEmail;
+  final String otherEmail;
 
-  ChatView(this.userEmail, {Key? key}) : super(key: key);
+  final User user;
+
+  const ChatView(this.user, this.otherEmail, {Key? key}) : super(key: key);
 
   @override
   State<ChatView> createState() => _ChatViewState();
 }
 
 class _ChatViewState extends State<ChatView> {
-  Chat chat = Chat();
+  final TextEditingController _textController = TextEditingController();
 
   @override
   Widget build(BuildContext context) => Scaffold(
-      appBar: AppBar(title: Text(widget.userEmail)), body: Container());
+        appBar: AppBar(title: Text(widget.otherEmail)),
+        body: SafeArea(
+          child: Column(
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: FutureBuilder(
+                    future: UserDbConnector.getMessagesOf(
+                            widget.user, widget.otherEmail)
+                        .then(Chat.fromList),
+                    builder: (_, AsyncSnapshot<Chat> snapshot) =>
+                        snapshot.hasData
+                            ? ListView.builder(
+                                itemCount: snapshot.requireData.messages.length,
+                                itemBuilder: (_, int _index) => MessageCard(
+                                    snapshot.requireData.messages[_index]),
+                              )
+                            : const Center(
+                                child: CircularProgressIndicator.adaptive()),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                  controller: _textController,
+                  decoration:
+                      const InputDecoration(hintText: "Inserire il messaggio"),
+                  maxLines: 5,
+                  minLines: 1,
+                ),
+              )
+            ],
+          ),
+        ),
+      );
 }
