a7e88732d41fe50f0065cb7c8f23c78cb436c644
leomiglio02 <56474420+leomiglio02@users.noreply.github.com>
2022-08-29 21:19:55 +0200

    modified chat.maxMessage with message

--- lib/View/segnalazioni/user/chat/chat_card.dart
+++ lib/View/segnalazioni/user/chat/chat_card.dart
@@ -1,39 +1,35 @@
 import 'package:flutter/material.dart';
 
-import '/Model/chat/chat.dart';
+import '/Model/chat/message.dart';
 
 /// La singola card di una chat
 class ChatCard extends StatelessWidget {
-  final Chat chat;
+  final Message showingMessage;
 
-  const ChatCard(this.chat, {Key? key}) : super(key: key);
+  const ChatCard(this.showingMessage, {Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) => 
-        Padding(
-          padding: const EdgeInsets.all(4),
-          child: Card(
-            elevation: 8,
-            child: Padding(
-              padding: const EdgeInsets.all(16),
-              child: Column(children: [
-                Row(children: [
-                  Text(chat.getMaxMessage().otherEmail),
-                  const Spacer(),
-                  Text(chat.getMaxMessage().orario),
-                ]),
-                const Text(''),
+  Widget build(BuildContext context) => Padding(
+        padding: const EdgeInsets.all(4),
+        child: Card(
+          elevation: 8,
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child:
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              Row(children: [
                 Text(
-                  (chat.getMaxMessage().sender
-                          ? chat.getMaxMessage().otherEmail
-                          : "Tu") +
-                      ": " +
-                      chat.getMaxMessage().text,
-                  maxLines: 3,
-                  softWrap: true,
+                  showingMessage.otherEmail,
+                  style: const TextStyle(fontWeight: FontWeight.bold),
                 ),
+                const Spacer(),
+                Text(showingMessage.orario.substring(10)),
               ]),
-            ),
+              const Text(''),
+              Text((showingMessage.sender ? "" : "Tu: ") + showingMessage.text,
+                  maxLines: 3, textAlign: TextAlign.left),
+            ]),
           ),
-        );
+        ),
+      );
 }
