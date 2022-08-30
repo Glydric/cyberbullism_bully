9d72620be5100450d8cf8dedd64d74454232dd56
leomiglio02 <56474420+leomiglio02@users.noreply.github.com>
2022-08-30 11:55:38 +0200

    minor

--- lib/View/segnalazioni/user/chat/message_card.dart
+++ lib/View/segnalazioni/user/chat/message_card.dart
@@ -8,25 +8,24 @@ class MessageCard extends StatelessWidget {
   const MessageCard(this.message, {Key? key}) : super(key: key);
 
   get chipColor => const Color.fromARGB(16, 0, 0, 0);
+
   get messageAlignment =>
       message.sender ? Alignment.centerLeft : Alignment.centerRight;
 
   @override
-  Widget build(BuildContext context) => Column(
-        children: [
-          Text(
-            "\n" + message.orario + "\n",
-            textAlign: TextAlign.left,
-            style: const TextStyle(fontSize: 10),
+  Widget build(BuildContext context) => Column(children: [
+        Text(
+          "\n" + message.orario + "\n",
+          textAlign: TextAlign.left,
+          style: const TextStyle(fontSize: 10),
+        ),
+        Align(
+          alignment: messageAlignment,
+          child: Chip(
+            backgroundColor: chipColor,
+            elevation: 4,
+            label: Text(message.text),
           ),
-          Align(
-            alignment: messageAlignment,
-            child: Chip(
-              backgroundColor: chipColor,
-              elevation: 4,
-              label: Text(message.text),
-            ),
-          )
-        ],
-      );
+        )
+      ]);
 }
