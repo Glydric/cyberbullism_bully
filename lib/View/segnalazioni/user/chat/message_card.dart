17b826bf87ffbafbda99cfa8054eb879b7039fe3
leomiglio02 <56474420+leomiglio02@users.noreply.github.com>
2022-08-31 12:28:37 +0200

    improvement to readability and design, now text cannot over 500 characters

--- lib/View/segnalazioni/user/chat/message_card.dart
+++ lib/View/segnalazioni/user/chat/message_card.dart
@@ -5,14 +5,17 @@ import '/Model/chat/message.dart';
 class MessageCard extends StatelessWidget {
   final Message message;
 
-  final test = false;
-
   const MessageCard(this.message, {Key? key}) : super(key: key);
 
   get cardColor => const Color.fromARGB(255, 233, 233, 233);
 
   get messageAlignment =>
       message.sender ? Alignment.centerLeft : Alignment.centerRight;
+
+  get marginAlignments => EdgeInsets.only(
+        left: message.sender ? 0 : 50,
+        right: message.sender ? 50 : 0,
+      );
 
   @override
   Widget build(BuildContext context) => Column(children: [
@@ -24,19 +27,19 @@ class MessageCard extends StatelessWidget {
         Align(
           alignment: messageAlignment,
           child: Card(
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(360)),
-                  color: cardColor,
-                  elevation: 4,
-                  child: Padding(
-                    padding:
-                        const EdgeInsets.symmetric(horizontal: 12, vertical: 7),
-                    child: Text(
-                      message.text,
-                      style: Theme.of(context).textTheme.bodyText1,
-                    ),
-                  ),
-                ),
+            margin: marginAlignments,
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+            color: cardColor,
+            elevation: 4,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 7),
+              child: Text(
+                message.text,
+                style: Theme.of(context).textTheme.bodyText1,
+              ),
+            ),
+          ),
         )
       ]);
 }
