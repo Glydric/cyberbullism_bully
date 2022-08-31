34027aa31afe25d65357b34635c54ae81d06151e
leomiglio02 <56474420+leomiglio02@users.noreply.github.com>
2022-08-31 12:28:52 +0200

    simplified and added refresh capability

--- lib/View/segnalazioni/user/chat/chat_view.dart
+++ lib/View/segnalazioni/user/chat/chat_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import '/Model/user.dart';
 import '/Model/connect_db/user_db_connector.dart';
@@ -7,9 +8,8 @@ import 'message_card.dart';
 
 /// La schermata che consente di portare avanti una chat con un utente
 class ChatView extends StatefulWidget {
-  final String otherEmail;
-
   final User user;
+  final String otherEmail;
 
   const ChatView(this.user, this.otherEmail, {Key? key}) : super(key: key);
 
@@ -20,42 +20,75 @@ class ChatView extends StatefulWidget {
 class _ChatViewState extends State<ChatView> {
   final TextEditingController _textController = TextEditingController();
 
+  Future<Chat> get messages =>
+      UserDbConnector.getMessagesOf(widget.user, widget.otherEmail)
+          .then(Chat.fromList);
+
+  get _maxTextLength => 500;
+
+  get errorText => _textController.text.length == _maxTextLength
+      ? "Impossibile inserire altri caratteri"
+      : null;
+
+  void updateChat() => setState(() {
+        messages;
+      });
+
+  void send() {
+    //TODO inviare il messaggio
+    _textController.clear;
+    updateChat();
+  }
+
   @override
   Widget build(BuildContext context) => Scaffold(
         appBar: AppBar(title: Text(widget.otherEmail)),
         body: SafeArea(
-          child: Column(
-            children: [
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: FutureBuilder(
-                    future: UserDbConnector.getMessagesOf(
-                            widget.user, widget.otherEmail)
-                        .then(Chat.fromList),
-                    builder: (_, AsyncSnapshot<Chat> snapshot) =>
-                        snapshot.hasData
-                            ? ListView.builder(
-                                itemCount: snapshot.requireData.messages.length,
-                                itemBuilder: (_, int _index) => MessageCard(
-                                    snapshot.requireData.messages[_index]),
-                              )
-                            : Container(),
-                  ),
+          child: Column(children: [
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: FutureBuilder(
+                  future: messages,
+                  builder: (_, AsyncSnapshot<Chat> snapshot) => snapshot.hasData
+                      ? ListView.builder(
+                          itemCount: snapshot.requireData.messages.length,
+                          itemBuilder: (_, int _index) => MessageCard(
+                              snapshot.requireData.messages[_index]),
+                        )
+                      : Container(),
                 ),
               ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextField(
-                  controller: _textController,
-                  decoration:
-                      const InputDecoration(hintText: "Inserire il messaggio"),
-                  maxLines: 5,
-                  minLines: 1,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                onChanged: (s) => setState(() => errorText),
+                inputFormatters: [
+                  LengthLimitingTextInputFormatter(_maxTextLength)
+                ],
+                textAlignVertical: TextAlignVertical.center,
+                controller: _textController,
+                decoration: InputDecoration(
+                  errorText: errorText,
+                  contentPadding: const EdgeInsets.symmetric(
+                    horizontal: 8,
+                    vertical: 4,
+                  ),
+                  border: const OutlineInputBorder(),
+                  hintText: "Inserire il messaggio",
+                  suffixIcon: IconButton(
+                    splashRadius: 20,
+                    color: Theme.of(context).primaryColor,
+                    icon: const Icon(Icons.send),
+                    onPressed: send,
+                  ),
                 ),
-              )
-            ],
-          ),
+                maxLines: 4,
+                minLines: 1,
+              ),
+            ),
+          ]),
         ),
       );
 }
