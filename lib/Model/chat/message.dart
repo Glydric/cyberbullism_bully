ee1f9216d8b5c2230bd38138790e97a65e05e85f
leomiglio02 <56474420+leomiglio02@users.noreply.github.com>
2022-09-02 19:15:22 +0200

    added date getter

--- lib/Model/chat/message.dart
+++ lib/Model/chat/message.dart
@@ -15,6 +15,14 @@ class Message {
         data = DateTime.parse(json["data"]),
         sender = json["sender"] == "1";
 
-  String get getHourMinutes =>
-      data.hour.toString() + ":" + data.minute.toString();
+  String get hourMinutes => data.hour.toString() + ":" + data.minute.toString();
+
+  String get yearMonthDate => DateFormat.yMMMd().format(data);
+
+  String get dayMonth =>
+      data.timeZoneName +
+      "/" +
+      data.month.toString() +
+      "/" +
+      data.day.toString();
 }
