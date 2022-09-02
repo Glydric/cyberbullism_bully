301d17abcefbfa7b138ac26601930b27ba9e0c00
leomiglio02 <56474420+leomiglio02@users.noreply.github.com>
2022-09-02 19:17:16 +0200

    used Dateformat for getter

--- lib/Model/chat/message.dart
+++ lib/Model/chat/message.dart
@@ -15,14 +15,7 @@ class Message {
         data = DateTime.parse(json["data"]),
         sender = json["sender"] == "1";
 
-  String get hourMinutes => data.hour.toString() + ":" + data.minute.toString();
+  String get hourMinutes => DateFormat.Hm().format(data);
 
   String get yearMonthDate => DateFormat.yMMMd().format(data);
-
-  String get dayMonth =>
-      data.timeZoneName +
-      "/" +
-      data.month.toString() +
-      "/" +
-      data.day.toString();
 }
