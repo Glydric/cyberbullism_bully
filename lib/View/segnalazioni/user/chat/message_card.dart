dc78d956e598c4484662e55719f82a1b3ef9cab1
leomiglio02 <56474420+leomiglio02@users.noreply.github.com>
2022-09-03 11:30:49 +0200

    added isLast paramether

--- lib/View/segnalazioni/user/chat/message_card.dart
+++ lib/View/segnalazioni/user/chat/message_card.dart
@@ -4,9 +4,14 @@ import '/Model/chat/message.dart';
 class MessageCard extends StatelessWidget {
   final Message message;
   final bool showDate;
+  final bool isLast;
 
-  const MessageCard(this.message, {Key? key, this.showDate = true})
-      : super(key: key);
+  const MessageCard(
+    this.message, {
+    Key? key,
+    this.showDate = true,
+    this.isLast = false,
+  }) : super(key: key);
 
   Color get cardColor => const Color.fromARGB(255, 233, 233, 233);
 
@@ -16,54 +21,62 @@ class MessageCard extends StatelessWidget {
   CrossAxisAlignment get crossTextAlignment =>
       message.sender ? CrossAxisAlignment.start : CrossAxisAlignment.end;
 
-  TextAlign get textAlignment =>
-      message.sender ? TextAlign.left : TextAlign.right;
-
   EdgeInsets get marginAlignments => EdgeInsets.only(
         left: message.sender ? 0 : 50,
         right: message.sender ? 50 : 0,
+      );
+
+  EdgeInsets get lastPadding =>
+      const EdgeInsets.symmetric(vertical: 4, horizontal: 8).copyWith(
+        bottom: isLast ? 16 : 0,
       );
 
   @override
   Widget build(BuildContext context) => Column(children: [
         Visibility(
           visible: showDate,
-          child: Text(
-            "\n" + message.yearMonthDate,
-            textAlign: TextAlign.left,
-            style: const TextStyle(fontSize: 10),
+          child: Padding(
+            padding: const EdgeInsets.only(top: 8),
+            child: Chip(
+              labelStyle: Theme.of(context)
+                  .textTheme
+                  .bodySmall!
+                  .copyWith(color: Colors.black),
+              label: Text( message.yearMonthDate),
+            ),
           ),
         ),
         Padding(
-          padding: const EdgeInsets.symmetric(vertical: 4.0, horizontal: 8),
+          padding: lastPadding,
           child: Align(
-              alignment: messageAlignment,
-              child: Column(
-                crossAxisAlignment: crossTextAlignment,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(4.0),
+            alignment: messageAlignment,
+            child: Column(
+              crossAxisAlignment: crossTextAlignment,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(4.0),
+                  child: Text(
+                    message.hourMinutes,
+                    style: Theme.of(context).textTheme.labelSmall,
+                  ),
+                ),
+                Card(
+                  margin: marginAlignments,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(14)),
+                  color: cardColor,
+                  elevation: 4,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8),
                     child: Text(
-                      message.hourMinutes,
-                      style: Theme.of(context).textTheme.labelSmall,
+                      message.text,
+                      style: Theme.of(context).textTheme.bodyText1,
                     ),
                   ),
-                  Card(
-                    margin: marginAlignments,
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(14)),
-                    color: cardColor,
-                    elevation: 4,
-                    child: Padding(
-                      padding: const EdgeInsets.all(8),
-                      child: Text(
-                        message.text,
-                        style: Theme.of(context).textTheme.bodyText1,
-                      ),
-                    ),
-                  ),
-                ],
-              )),
+                )
+              ],
+            ),
+          ),
         ),
       ]);
 }
