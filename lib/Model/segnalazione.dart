72c17b8660068cd6cecd20028c3267290c9c45d0
leomiglio02 <56474420+leomiglio02@users.noreply.github.com>
2022-09-05 15:58:41 +0200

    modified date string with datetime

--- lib/Model/segnalazione.dart
+++ lib/Model/segnalazione.dart
@@ -1,14 +1,19 @@
+import 'package:intl/intl.dart';
+
 class Segnalazione {
   String email;
   String testo;
-  String data;
   int gravita;
+  DateTime data;
 
   Segnalazione(this.testo, this.data, this.gravita, this.email);
-  
+
   Segnalazione.fromJson(Map<String, dynamic> json)
       : email = json["email"],
         testo = json["testo"],
-        data = json["data"],
+        data = DateTime.parse(json["data"]),
         gravita = int.parse(json["gravita"]);
+
+  String get yearMonth => DateFormat.yMd().format(data);
+  String get hourDay => DateFormat.Hm().format(data);
 }
