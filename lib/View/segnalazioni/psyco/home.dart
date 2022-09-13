8daaadd9fb5c4279c6c19f6c40307ec43a1396b4
leomiglio02 <56474420+Glydric22@users.noreply.github.com>
2022-09-13 15:13:10 +0200

    resolved error

--- lib/View/segnalazioni/psyco/home.dart
+++ lib/View/segnalazioni/psyco/home.dart
@@ -1,5 +1,9 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
+import '../../../Model/chat/chat.dart';
+import '../../../Model/connect_db/psyco_db_connector.dart';
 import '/Model/user.dart';
 import 'chat/psyco_chat_list.dart';
 import 'segnalazione_list.dart';
@@ -14,12 +18,37 @@ class PsycoSegnalazioni extends StatefulWidget {
 }
 
 class _PsycoSegnalazioniState extends State<PsycoSegnalazioni> {
+  late final Timer timer;
+
+  ///ottiene la lista degli ultimi messaggi
+  get chats => PsycoDbConnector.getLastMessages(widget.user)
+      .then((messages) => messages.map(Chat.singleMessage).toList());
+
+  void updateChat() => setState(() {
+        chats;
+      });
+
+  @override
+  void dispose() {
+    timer.cancel();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    timer = Timer.periodic(
+      const Duration(seconds: 1),
+      (_) => updateChat(),
+    );
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) => Scaffold(
       floatingActionButton: FloatingActionButton(
         onPressed: () => toPage(ListaSegnalazioni(widget.user)),
       ),
-      body: PsycoChatList(widget.user));
+      body: PsycoChatList(widget.user, chats));
 
   toPage(Widget page) => Navigator.of(context).push(MaterialPageRoute(
         builder: (context) => page,
