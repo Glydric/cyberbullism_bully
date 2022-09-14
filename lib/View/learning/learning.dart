71a6376a94aa77ae87d7353042b242ea4731c1f6
leomiglio02 <56474420+Glydric22@users.noreply.github.com>
2022-09-14 14:28:08 +0200

    implemented

--- lib/View/learning/learning.dart
+++ lib/View/learning/learning.dart
@@ -2,17 +2,17 @@ import 'package:cyberbullism_bully/Model/learning/get_learnings.dart';
 import 'package:cyberbullism_bully/View/learning/learning_grid.dart';
 import 'package:flutter/material.dart';
 
-import 'learning_card.dart';
+import '/Model/learning/learning_element.dart';
 
 class LearningPage extends StatelessWidget {
   const LearningPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) => Scaffold(
-    floatingActionButton: FloatingActionButton(onPressed: () => GetLearnings.elements()),
-        appBar: AppBar(title: const Text("Learning Page")),
-        body: LearningGrid(getListOfLearning),
+  Widget build(BuildContext context) => FutureBuilder(
+        future: GetLearnings.elements,
+        builder: (_, AsyncSnapshot<List<LearningElement>> snapshot) => Scaffold(
+          appBar: AppBar(title: const Text("Learning Page")),
+          body: LearningGrid(snapshot.requireData),
+        ),
       );
-
-  List<LearningCard> get getListOfLearning => [LearningCard()]; //TODO implement
 }
