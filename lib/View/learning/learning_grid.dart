d8c125397678ef9f977906cfc343c292863ad2f4
leomiglio02 <56474420+Glydric22@users.noreply.github.com>
2022-09-14 14:27:56 +0200

    used learning element list

--- lib/View/learning/learning_grid.dart
+++ lib/View/learning/learning_grid.dart
@@ -1,10 +1,12 @@
 import 'package:cyberbullism_bully/View/learning/learning_card.dart';
 import 'package:flutter/material.dart';
 
-class LearningGrid extends StatefulWidget {
-  final List<LearningCard> _grid;
+import '/Model/learning/learning_element.dart';
 
-  const LearningGrid(this._grid, {Key? key}) : super(key: key);
+class LearningGrid extends StatefulWidget {
+  final List<LearningElement> _list;
+
+  const LearningGrid(this._list, {Key? key}) : super(key: key);
 
   @override
   State<StatefulWidget> createState() => _LearningGridState();
@@ -19,7 +21,10 @@ class _LearningGridState extends State<LearningGrid> {
           crossAxisSpacing: 5,
         ),
         padding: const EdgeInsets.all(20),
-        itemCount: widget._grid.length,
-        itemBuilder: (context, int index) => widget._grid[index],
+        itemCount: widget._list.length,
+        itemBuilder: (context, int index) => learnings[index],
       );
+
+  List<LearningCard> get learnings =>
+      widget._list.map(LearningCard.new).toList();
 }
