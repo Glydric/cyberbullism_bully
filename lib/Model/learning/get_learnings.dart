dd2e7273f2ae07411968bd02137b2d22f71623b8
leomiglio02 <56474420+Glydric22@users.noreply.github.com>
2022-09-14 16:02:24 +0200

    added learning getting

--- lib/Model/learning/get_learnings.dart
+++ lib/Model/learning/get_learnings.dart
@@ -1,18 +1,35 @@
+import 'dart:async';
 import 'dart:convert';
 
 import 'package:cyberbullism_bully/Model/learning/learning_element.dart';
 import 'package:flutter/services.dart';
 
 class GetLearnings {
-  static Future<List<LearningElement>> get learnings async => json
-      .decode(await rootBundle.loadString('AssetManifest.json'))
-      .keys
-      .where((String key) => key.contains("learning_resources"))
-      // .map(print)
-      .map(element)
-      .map((e) async => await e)
-      .toList();
+  static Future<List<LearningElement>> get learnings async =>
+      getFutureKeys.then(getElements).then(cleanElements);
 
-  static Future<LearningElement> element(String filePath) =>
-      rootBundle.loadString(filePath).then(LearningElement.new);
+  static Future<dynamic> get getFutureAssets async =>
+      getElement('AssetManifest.json').then((value) => json.decode(value));
+
+  static Future<List<String>> get getFutureKeys => getFutureAssets.then(
+        (value) => value.keys
+            .where((String key) => key.contains("learning_resources"))
+            .toList(),
+      );
+
+  static Future<List<Future<LearningElement>>> getElements(
+          List<String> fileList) async =>
+      fileList.map(getElement).map((e) => e.then(LearningElement.new)).toList();
+
+  static Future<String> getElement(String filePath) =>
+      rootBundle.loadString(filePath);
+
+  static Future<List<LearningElement>> cleanElements(
+      List<Future<LearningElement>> list) async {
+    List<LearningElement> result = [];
+    for (Future<LearningElement> element in list) {
+      result.add(await element);
+    }
+    return result;
+  }
 }
