269bf28ff63f5ae3874c3224a01b8647d2678a6b
leomiglio02 <56474420+Glydric22@users.noreply.github.com>
2022-09-14 14:27:16 +0200

    added asset to get learnings

--- lib/Model/learning/get_learnings.dart
+++ lib/Model/learning/get_learnings.dart
@@ -1,15 +1,16 @@
+import 'dart:convert';
 
 import 'package:cyberbullism_bully/Model/learning/learning_element.dart';
 import 'package:flutter/services.dart';
 import 'package:path/path.dart' as p;
 
 const assetsPath = "assets/learning_resources";
+get assets async => jsonDecode(await rootBundle.loadString('AssetManifest.json'));
 
 class GetLearnings {
-  // Future<List<LearningElement>> get elements async {
-  static void elements() =>
-    rootBundle
-        .loadString(p.join(assetsPath, "Bullism.txt"))
-        .then(LearningElement.new);
-  
+  static Future<List<LearningElement>> get elements => assets.key;
+
+  static Future<LearningElement> element(String fileName) => rootBundle
+      .loadString(p.join(assetsPath, fileName))
+      .then(LearningElement.new);
 }
