7f5dae5d25bfcd279e007f78061b8962669acc1f
leomiglio02 <56474420+Glydric22@users.noreply.github.com>
2022-09-20 19:14:29 +0200

    no error when opening app, using futureBuilder

--- lib/main_switcher.dart
+++ lib/main_switcher.dart
@@ -9,6 +9,8 @@ import 'package:cyberbullism_bully/View/segnalazioni/switcher.dart';
 import 'package:cyberbullism_bully/View/profile/home_page.dart';
 import 'package:cyberbullism_bully/View/learning/learning.dart';
 
+import 'Model/user.dart';
+
 class ScreenSwitcher extends StatefulWidget {
   const ScreenSwitcher({Key? key}) : super(key: key);
 
@@ -19,11 +21,6 @@ class ScreenSwitcher extends StatefulWidget {
 class _ScreenSwitcherState extends State<ScreenSwitcher> {
   int _index = 0;
   late Timer timer;
-  late Map<Widget, NavigationDestination> navigatorMap;
-
-  List<Widget> get getPages => navigatorMap.keys.toList();
-
-  List<NavigationDestination> get getItems => navigatorMap.values.toList();
 
   @override
   void dispose() {
@@ -34,31 +31,42 @@ class _ScreenSwitcherState extends State<ScreenSwitcher> {
   @override
   void initState() {
     timer = Timer.periodic(
-      const Duration(milliseconds: 100),
-      (_) => updateNav(),
+      const Duration(milliseconds: 200),
+      (_) => setState(() {}),
     );
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) => FutureBuilder(
-        future: UserSavingManager.getUser(),
-        builder: (_, AsyncSnapshot snapshot) => Scaffold(
-          body: getPages[_index],
+      future: UserSavingManager.getUser(),
+      builder: (_, AsyncSnapshot<User> snapshot) {
+        final navigator = snapshot.hasError || !snapshot.hasData
+            ? {
+                const HomePage(): const NavigationDestination(
+                  label: "User",
+                  icon: Icon(Icons.account_circle),
+                ),
+                const LearningPage(): const NavigationDestination(
+                  label: "Learning",
+                  icon: Icon(Icons.description),
+                ),
+              }
+            : getNavBasedOn(snapshot.requireData);
+        return Scaffold(
+          body: navigator.keys.elementAt(_index),
           bottomNavigationBar: NavigationBar(
             onDestinationSelected: _updateIndex,
             selectedIndex: _index,
-            destinations: getItems,
+            destinations: navigator.values.toList(),
           ),
-        ),
-      );
+        );
+      });
 
   _updateIndex(int value) => setState(() => _index = value);
 
-  updateNav() async {
-    try {
-      final user = await UserSavingManager.getUser();
-      navigatorMap = user.runtimeType.toString() == "Psyco"
+  Map<Widget, NavigationDestination> getNavBasedOn(User user) =>
+      user.runtimeType.toString() == "Psyco"
           ? {
               const HomePage(): const NavigationDestination(
                 label: "User",
@@ -91,19 +99,4 @@ class _ScreenSwitcherState extends State<ScreenSwitcher> {
                 icon: Icon(Icons.description),
               ),
             };
-    } catch (e) {
-      navigatorMap = {
-        const HomePage(): const NavigationDestination(
-          label: "User",
-          icon: Icon(Icons.account_circle),
-        ),
-        const LearningPage(): const NavigationDestination(
-          label: "Learning",
-          icon: Icon(Icons.description),
-        ),
-      };
-    } finally {
-      setState(() => navigatorMap);
-    }
-  }
 }
