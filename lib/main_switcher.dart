4b5ad0eaf88bd209970077c3432a934f14c1a035
leomiglio02 <56474420+Glydric22@users.noreply.github.com>
2022-09-20 17:30:55 +0200

    added new navigator button if psyco

--- lib/main_switcher.dart
+++ lib/main_switcher.dart
@@ -1,10 +1,12 @@
 import 'package:cyberbullism_bully/Model/user_save_manager.dart';
+import 'package:cyberbullism_bully/View/segnalazioni/psyco/lista_segnalazione.dart';
 import 'package:flutter/material.dart';
 
 // importo pagine flutter
 import 'package:cyberbullism_bully/View/segnalazioni/switcher.dart';
 import 'package:cyberbullism_bully/View/profile/home_page.dart';
 import 'package:cyberbullism_bully/View/learning/learning.dart';
+import 'package:http/http.dart';
 
 class ScreenSwitcher extends StatefulWidget {
   const ScreenSwitcher({Key? key}) : super(key: key);
@@ -16,20 +18,7 @@ class ScreenSwitcher extends StatefulWidget {
 class _ScreenSwitcherState extends State<ScreenSwitcher> {
   int _index = 0;
 
-  final Map<Widget, NavigationDestination> navigatorMap = {
-    const LearningPage(): const NavigationDestination(
-      label: "Learning",
-      icon: Icon(Icons.description),
-    ),
-    const SegnalazioniPage(): const NavigationDestination(
-      label: "Chat",
-      icon: Icon(Icons.chat),
-    ),
-    const HomePage(): const NavigationDestination(
-      label: "User",
-      icon: Icon(Icons.account_circle),
-    )
-  };
+  late Map<Widget, NavigationDestination> navigatorMap;
 
   List<Widget> get getPages => navigatorMap.keys.toList();
 
@@ -54,10 +43,49 @@ class _ScreenSwitcherState extends State<ScreenSwitcher> {
           ),
         ),
       );
-  _updateIndex(int value) => setState(() => _index = value);
-
-  void checkPsy() async {
-    
+  _updateIndex(int value) {
+    checkPsy();
+    setState(() => _index = value);
   }
 
+  checkPsy() async {
+    try {
+      final user = await UserSavingManager.getUser();
+      if (user.runtimeType.toString() == "Psyco") {
+        navigatorMap = {
+          const LearningPage(): const NavigationDestination(
+            label: "Learning",
+            icon: Icon(Icons.description),
+          ),
+          const SegnalazioniPage(): const NavigationDestination(
+            label: "Chat",
+            icon: Icon(Icons.chat),
+          ),
+          ListaSegnalazioni(user): const NavigationDestination(
+              label: "Segnalazioni", icon: Icon(Icons.list)),
+          const HomePage(): const NavigationDestination(
+            label: "User",
+            icon: Icon(Icons.account_circle),
+          ),
+        };
+      } else {
+        throw Exception();
+      }
+    } catch (e) {
+      navigatorMap = {
+        const LearningPage(): const NavigationDestination(
+          label: "Learning",
+          icon: Icon(Icons.description),
+        ),
+        const SegnalazioniPage(): const NavigationDestination(
+          label: "Chat",
+          icon: Icon(Icons.chat),
+        ),
+        const HomePage(): const NavigationDestination(
+          label: "User",
+          icon: Icon(Icons.account_circle),
+        )
+      };
+    }
+  }
 }
