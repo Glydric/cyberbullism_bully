fd17cd0e199b0160e2359883d1d8912423cabac8
leomiglio02 <56474420+Glydric22@users.noreply.github.com>
2022-09-21 19:26:02 +0200

    added gravity

--- lib/Model/chat/message.dart
+++ lib/Model/chat/message.dart
@@ -5,14 +5,16 @@ class Message {
   final String text;
   final bool sender;
   final DateTime data;
+  int? gravita;
 
-  Message(this.otherEmail, this.text, this.data, this.sender);
+  Message(this.otherEmail, this.text, this.data, this.sender, this.gravita);
 
   Message.fromJson(Map<String, dynamic> json)
       : otherEmail = json["otherEmail"],
         text = json["testo"],
         data = DateTime.parse(json["data"]),
-        sender = json["sender"] == "1";
+        sender = json["sender"] == "1",
+        gravita = json["gravita"] == null ? null : int.parse(json["gravita"]);
 
   String get hourMinutes => DateFormat.Hm().format(data);
 
