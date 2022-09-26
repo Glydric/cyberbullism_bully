e6fe3361bc6670534aa6d0985af8f95ea74477b0
leomiglio02 <56474420+Glydric22@users.noreply.github.com>
2022-09-26 19:42:49 +0200

    extended ChatView

--- lib/View/chat/psyco/chat_view.dart
+++ lib/View/chat/psyco/chat_view.dart
@@ -1,134 +1,40 @@
 import 'dart:async';
 
-import 'package:cyberbullism_bully/Model/connect_db/psyco_db_connector.dart';
-import 'package:cyberbullism_bully/Model/user.dart';
+import 'package:cyberbullism_bully/View/chat/user/chat_view.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
-import '/View/connection_error_ui.dart';
-import '../message_card.dart';
-import '/Model/chat/chat.dart';
+import 'package:cyberbullism_bully/Model/user.dart';
+import 'package:cyberbullism_bully/Model/connect_db/psyco_db_connector.dart';
+import 'package:cyberbullism_bully/Model/chat/chat.dart';
 
-int get _maximumTextLength => 500;
 
 /// La schermata che consente di portare avanti una chat con un utente
-class PsycoChatView extends StatefulWidget {
-  final User user;
-  final String otherEmail;
+class PsycoChatView extends ChatView {
 
-  const PsycoChatView(this.user, this.otherEmail, {Key? key}) : super(key: key);
+  const PsycoChatView(User user, String otherEmail, {Key? key}) : super(user, otherEmail,key: key);
 
   @override
-  State<PsycoChatView> createState() => PsycoChatViewState();
+  // ignore: no_logic_in_create_state
+  ChatViewState createState() => PsycoChatViewState();
 }
 
-class PsycoChatViewState extends State<PsycoChatView> {
-  final TextEditingController _textController = TextEditingController();
-  late final Timer timer;
+class PsycoChatViewState extends ChatViewState {
 
+  @override
   Future<Chat> get messages =>
       PsycoDbConnector.getMessagesOf(widget.user, widget.otherEmail)
           .then(Chat.fromList);
 
-  get errorText => _textController.text.length == _maximumTextLength
-      ? "Impossibile inserire altri caratteri"
-      : null;
-
-  ///Fornisce il valore da inserire nel campo maxLength cosi da mostrare il massimo all'occorrenza
-  get seeMaxLength => _textController.text.length > _maximumTextLength - 50
-      ? _maximumTextLength
-      : null;
-
-  void updateChat() => setState(() {
-        messages;
-      });
-
+  @override
   void send() {
-    PsycoDbConnector.sendMessage(
-      widget.user,
-      widget.otherEmail,
-      _textController.text,
-    );
-    _textController.clear();
-    updateChat();
-  }
-
-  @override
-  void dispose() {
-    timer.cancel();
-    _textController.dispose();
-    super.dispose();
-  }
-
-  @override
-  void initState() {
-    timer = Timer.periodic(
-      const Duration(milliseconds: 100),
-      (_) => updateChat(),
-    );
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) => Scaffold(
-        appBar: AppBar(title: Text(widget.otherEmail)),
-        body: SafeArea(
-          child: Column(children: [
-            Expanded(
-              child: FutureBuilder(
-                future: messages,
-                builder: (_, AsyncSnapshot<Chat> snapshot) => snapshot.hasError
-                    ? const ConnectionErrorUI()
-                    : snapshot.hasData
-                        ? ListView.builder(
-                            reverse: true,
-                            itemCount: snapshot.requireData.messages.length,
-                            itemBuilder: (_, int _index) => MessageCard(
-                              snapshot.requireData.messages[_index],
-                              showDate: showDate(snapshot.requireData, _index),
-                              isLast: _index == 0,
-                            ),
-                          )
-                        : const CircularProgressIndicator.adaptive(),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 8.0),
-              child: TextField(
-                maxLength: seeMaxLength,
-                onChanged: (s) => setState(() => errorText),
-                inputFormatters: [
-                  LengthLimitingTextInputFormatter(_maximumTextLength)
-                ],
-                textAlignVertical: TextAlignVertical.center,
-                controller: _textController,
-                decoration: InputDecoration(
-                  hintText: "Inserire il messaggio",
-                  helperText:
-                      "", // fornisce uno spazio in modo che all'aggiunta degli altri elementi il textfield non si alzi
-                  errorText: errorText,
-                  contentPadding: const EdgeInsets.symmetric(
-                    horizontal: 8,
-                    vertical: 4,
-                  ),
-                  border: const OutlineInputBorder(),
-                  suffixIcon: IconButton(
-                    splashRadius: 20,
-                    color: Theme.of(context).primaryColor,
-                    icon: const Icon(Icons.send),
-                    onPressed: send,
-                  ),
-                ),
-                maxLines: 4,
-                minLines: 1,
-              ),
-            ),
-          ]),
-        ),
+    if (super.textController.text == "") {
+      PsycoDbConnector.sendMessage(
+        widget.user,
+        widget.otherEmail,
+        textController.text,
       );
-
-  bool showDate(Chat chat, int _index) =>
-      _index + 1 == chat.messages.length ||
-      chat.messages[_index].yearMonthDate !=
-          chat.messages[_index + 1].yearMonthDate;
+      textController.clear();
+      updateChat();
+    }
+  }
 }
