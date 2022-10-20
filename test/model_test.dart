bff4a6ae737312effd9303d3a13b352d185b52dd
leomiglio02 <56474420+Glydric22@users.noreply.github.com>
2022-10-20 12:05:17 +0200

    tryed tests

--- test/model_test.dart
+++ test/model_test.dart
@@ -25,13 +25,16 @@ void main() {
 
     expectLater(learnings.isNotEmpty, true);
   });
-  test("User Websocket test", () async {
-    final user = User("l", "1", "l@1.it", "it");
+  /* test("User Websocket test", () async {
 
     final ws = UserWS(user, "p@1.it");
-    print(ws.messages);
-    expectLater(await ws.channel.stream.isEmpty, false);
-  });
+    final stream = ws.channel.stream;
+    final sink = ws.channel.sink;
+    stream.listen(onError: (event) => debugPrint(event));
+    sink.add("reload ");
+    // print(ws.messages);
+    // expectLater(await ws.channel.stream.isEmpty, false);
+  }); */
   /* test("User Save Manager Test", () async {
     User user = User("name", "surname", "name.surname@mail.it", "password");
 
--- test/view_test.dart
+++ test/view_test.dart
@@ -5,26 +5,40 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:cyberbullism_bully/Model/connect_db/web_socket/user.dart';
+import 'package:cyberbullism_bully/Model/user.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:cyberbullism_bully/main.dart';
 
 void main() {
-  testWidgets('Test Widget Example (Not Working)', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MainApp());
+  // testWidgets('Test Widget Example (Not Working)', (WidgetTester tester) async {
+  //   // Build our app and trigger a frame.
+  //   await tester.pumpWidget(const MainApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+  //   // Verify that our counter starts at 0.
+  //   expect(find.text('0'), findsOneWidget);
+  //   expect(find.text('1'), findsNothing);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+  //   // Tap the '+' icon and trigger a frame.
+  //   await tester.tap(find.byIcon(Icons.add));
+  //   await tester.pump();
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
-  });
+  //   // Verify that our counter has incremented.
+  //   expect(find.text('0'), findsNothing);
+  //   expect(find.text('1'), findsOneWidget);
+  // });
+  // testWidgets('Stream', (WidgetTester tester) async {
+  //   final ws = UserWS(
+  //     User("l", "1", "l@1.it", "it"),
+  //     "p@1.it",
+  //   );
+  //   // Build our app and trigger a frame.
+  //   await tester.pumpWidget(StreamBuilder(
+  //     stream: ws.channel.stream,
+  //     builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) =>
+  //         Text(snapshot.hasData ? '${snapshot.data}' : ''),
+  //   ));
+  // });
 }
