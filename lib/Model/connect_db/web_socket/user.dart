ad147facb8e26cf3c78155054cef1c146c9fe866
leomiglio02 <56474420+Glydric22@users.noreply.github.com>
2022-10-20 15:24:45 +0200

    minor

--- lib/Model/connect_db/web_socket/user.dart
+++ lib/Model/connect_db/web_socket/user.dart
@@ -1,31 +1,46 @@
+import 'dart:async';
 import 'dart:convert';
 
+import 'package:cyberbullism_bully/Model/chat/chat.dart';
 import 'package:cyberbullism_bully/Model/connect_db/login_exception.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
 
 import 'package:cyberbullism_bully/Model/user.dart';
-
 import 'package:cyberbullism_bully/Model/chat/message.dart';
 
-const url = 'ws://8080-glydric22-cyberbullismp-e1b2fmf3noc.ws-eu71.gitpod.io';
+const url =
+    'ws://8080-glydric22-cyberbullismp-e1b2fmf3noc.ws-eu71.gitpod.io/user';
 
 class UserWS {
-  final User user;
-  final String otherEmail;
-
   final channel = WebSocketChannel.connect(
     Uri.parse(url),
   );
 
-  UserWS(this.user, this.otherEmail);
+  UserWS(User user, String otherEmail) {
+    channel.sink.add("set " +
+        jsonEncode({
+          'email': user.email,
+          'password': user.password,
+          'otherEmail': otherEmail,
+        }));
+    update();
+  }
 
-  Future<List<Message>> get messages async {
-    final jsonValue = await channel.stream.last;
+  Stream<dynamic> get stream => channel.stream;
 
-    LoginException.thrower(jsonValue);
+  void update() => channel.sink.add("reload");
 
-    final List<dynamic> jsonList = jsonDecode(jsonValue);
+  void sendMessage(String message) => channel.sink.add("send " + message);
 
-    return jsonList.map((json) => Message.fromJson(json)).toList();
+  void close() => channel.sink.close();
+
+  Future<Chat> get messages async {
+    final body = await channel.stream.last;
+    
+    LoginException.thrower(body);
+
+    final List<dynamic> jsonList = jsonDecode(body);
+    final messages= jsonList.map((json) => Message.fromJson(json)).toList();
+    return Chat.fromList(messages);
   }
 }
