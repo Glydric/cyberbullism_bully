683742561017b2c9ab1c73893a66b16222951bc6
leomiglio02 <56474420+Glydric22@users.noreply.github.com>
2022-10-20 15:24:53 +0200

    middle working

--- lib/View/chat/user/chat_view.dart
+++ lib/View/chat/user/chat_view.dart
@@ -1,10 +1,12 @@
 import 'dart:async';
+import 'dart:convert';
 
+import 'package:cyberbullism_bully/Model/chat/message.dart';
+import 'package:cyberbullism_bully/Model/connect_db/web_socket/user.dart';
+import 'package:cyberbullism_bully/View/chat/message_card.dart';
+import 'package:cyberbullism_bully/View/connection_error_ui.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-
-import 'package:cyberbullism_bully/View/connection_error_ui.dart';
-import 'package:cyberbullism_bully/View/chat/message_card.dart';
 
 import 'package:cyberbullism_bully/Model/user.dart';
 import 'package:cyberbullism_bully/Model/connect_db/user_db_connector.dart';
@@ -26,8 +28,10 @@ class ChatView extends StatefulWidget {
 class ChatViewState extends State<ChatView> {
   final TextEditingController textController = TextEditingController();
   late final Timer timer;
+  late final UserWS ws;
 
   Future<Chat> get messages =>
+      // ws.messages
       UserDbConnector.getMessagesOf(widget.user, widget.otherEmail)
           .then(Chat.fromList);
 
@@ -42,6 +46,7 @@ class ChatViewState extends State<ChatView> {
 
   void updateChat() => setState(() {
         messages;
+        ws.update();
       });
 
   void sendMessage() {
@@ -64,78 +69,104 @@ class ChatViewState extends State<ChatView> {
   void dispose() {
     timer.cancel();
     textController.dispose();
+    ws.close();
     super.dispose();
   }
 
   @override
   void initState() {
+    ws = UserWS(widget.user, widget.otherEmail);
     timer = Timer.periodic(
       const Duration(milliseconds: 100),
       (_) => updateChat(),
     );
+
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) => Scaffold(
         appBar: AppBar(title: Text(widget.otherEmail)),
-        body: SafeArea(
-          child: Column(children: [
-            Expanded(
-              child: FutureBuilder(
-                future: messages,
-                builder: (_, AsyncSnapshot<Chat> snapshot) => snapshot.hasError
-                    ? const Center(child: ConnectionErrorUI())
-                    : snapshot.hasData
-                        ? ListView.builder(
-                            reverse: true,
-                            itemCount: snapshot.requireData.messages.length,
-                            itemBuilder: (_, int _index) => MessageCard(
-                              snapshot.requireData.messages[_index],
-                              showDate: showDate(snapshot.requireData, _index),
-                              isLast: _index == 0,
-                            ),
-                          )
-                        : const CircularProgressIndicator.adaptive(),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 8.0),
-              child: TextField(
-                maxLength: seeMaxLength,
-                onChanged: (s) => setState(() => errorText),
-                inputFormatters: [
-                  LengthLimitingTextInputFormatter(_maximumTextLength)
-                ],
-                textAlignVertical: TextAlignVertical.center,
-                controller: textController,
-                decoration: InputDecoration(
-                  hintText: "Inserire il messaggio",
-                  helperText:
-                      "", // fornisce uno spazio in modo che all'aggiunta degli altri elementi il textfield non si alzi
-                  errorText: errorText,
-                  contentPadding: const EdgeInsets.symmetric(
-                    horizontal: 8,
-                    vertical: 4,
-                  ),
-                  border: const OutlineInputBorder(),
-                  suffixIcon: IconButton(
-                    splashRadius: 20,
-                    color: Theme.of(context).primaryColor,
-                    icon: const Icon(Icons.send),
-                    onPressed: textController.text.isNotEmpty ? send : null,
-                  ),
+        body: Column(children: [
+          // Expanded(
+          //   child: FutureBuilder(
+          //     future: messages,
+          //     builder: (_, AsyncSnapshot<Chat> snapshot) => snapshot.hasError
+          //         ? const Center(child: ConnectionErrorUI())
+          //         : snapshot.hasData
+          //             ? ListView.builder(
+          //                 reverse: true,
+          //                 itemCount: snapshot.requireData.messages.length,
+          //                 itemBuilder: (_, int _index) => MessageCard(
+          //                   snapshot.requireData.messages[_index],
+          //                   showDate: showDate(snapshot.requireData, _index),
+          //                   isLast: _index == 0,
+          //                 ),
+          //               )
+          //             : const CircularProgressIndicator.adaptive(),
+          //   ),
+          // ),
+          StreamBuilder(
+            stream: ws.stream,
+            builder: (context, snapshot) => snapshot.hasError
+                ? Text(snapshot.toString())
+                : snapshot.hasData
+                    ? Text('${snapshot.data}')
+                    // listChatBuilder(snapshot.data)
+                    : const CircularProgressIndicator.adaptive(),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: TextField(
+              maxLength: seeMaxLength,
+              onChanged: (s) => setState(() => errorText),
+              inputFormatters: [
+                LengthLimitingTextInputFormatter(_maximumTextLength)
+              ],
+              textAlignVertical: TextAlignVertical.center,
+              controller: textController,
+              decoration: InputDecoration(
+                hintText: "Inserire il messaggio",
+                helperText:
+                    "", // fornisce uno spazio in modo che all'aggiunta degli altri elementi il textfield non si alzi
+                errorText: errorText,
+                contentPadding: const EdgeInsets.symmetric(
+                  horizontal: 8,
+                  vertical: 4,
                 ),
-                maxLines: 4,
-                minLines: 1,
+                border: const OutlineInputBorder(),
+                suffixIcon: IconButton(
+                  splashRadius: 20,
+                  color: Theme.of(context).primaryColor,
+                  icon: const Icon(Icons.send),
+                  onPressed: textController.text.isNotEmpty ? send : null,
+                ),
               ),
+              maxLines: 4,
+              minLines: 1,
             ),
-          ]),
-        ),
+          ),
+        ]),
       );
 
   bool showDate(Chat chat, int _index) =>
       _index + 1 == chat.messages.length ||
       chat.messages[_index].yearMonthDate !=
           chat.messages[_index + 1].yearMonthDate;
+
+  listChatBuilder(body) {
+    final List<dynamic> jsonList = jsonDecode(body);
+    final messages = jsonList.map((json) => Message.fromJson(json)).toList();
+    final chat = Chat.fromList(messages);
+
+    return ListView.builder(
+      reverse: true,
+      itemCount: chat.messages.length,
+      itemBuilder: (_, int _index) => MessageCard(
+        chat.messages[_index],
+        showDate: showDate(chat, _index),
+        isLast: _index == 0,
+      ),
+    );
+  }
 }
