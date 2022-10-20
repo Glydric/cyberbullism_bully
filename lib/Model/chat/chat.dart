026be68a94ae12b239003645719053cb6500c30d
leomiglio02 <56474420+Glydric22@users.noreply.github.com>
2022-10-20 15:33:45 +0200

    add from json string capability

--- lib/Model/chat/chat.dart
+++ lib/Model/chat/chat.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'message.dart';
 
 /// Una lista di messaggi con lo stesso psicologo
@@ -19,5 +20,10 @@ class Chat {
             .where((message) => message.otherEmail == messageList[0].otherEmail)
             .toList();
 
+  factory Chat.fromJsonString(String body) {
+    final List<dynamic> jsonList = jsonDecode(body);
+    final messages = jsonList.map((json) => Message.fromJson(json)).toList();
+    return Chat.fromList(messages);
+  }
   Message getMaxMessage() => messages[messages.length - 1];
 }
