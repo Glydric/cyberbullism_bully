82698aedc01131e90b8153724df5c9486b48579d
leomiglio02 <leo@miglio.dev>
2022-11-21 22:44:31 +0100

    removed tested commit

--- lib/Model/connect_db/web_socket/psyco.dart
+++ lib/Model/connect_db/web_socket/psyco.dart
@@ -3,7 +3,6 @@ import 'package:cyberbullism_bully/Model/user.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
 
 class PsycoWS extends UserWS {
-  // TODO! controlla funzionamento
   @override
   // ignore: overridden_fields
   final channel = WebSocketChannel.connect(
